c5c2a40b3360848ef53e8cd9bd46339680ea4372
rohail jan <rohailjan726@gmail.com>
2025-05-14 13:29:23 +0500

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,7 @@ import Projects from './components/Projects';
 import Technologies from './components/Technologies';
 import ContactUs from './components/ContactUs';
 import Visitus from './components/Visitus';
-import Choose from './components/Choose';
-import FAQ from './components/FAQ';
 import Home from './pages/Home';
-import Content from './components/Content';
-import CompanyOrg from './components/CompanyOrg';
-import Testimonial from './components/Testimonial';
 
 const App = () => {
   return (
@@ -28,11 +23,6 @@ const App = () => {
             <Route path="/technology" element={<Technologies />} />
             <Route path="/contact" element={<ContactUs />} />
             <Route path="/about" element={<Visitus />} />
-            <Route path="/choose" element={<Choose />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/testimonials" element={<Testimonial/>} />
-            <Route path="/content" element={<Content />} />
-            <Route path="/companyorg" element={<CompanyOrg/>} />
           </Routes>
         </div>
         <Footer />
--- src/components/Routes/allroutes.jsx
+++ src/components/Routes/allroutes.jsx
@@ -1,6 +1,4 @@
 import Home from "../../pages/Home";
-import Content from "../Content";
-
 export const allroutes = [
   { path: '/', element: <Home/> }, // Home should show Hero
   { path: '/services', element: <Services /> },
@@ -8,11 +6,4 @@ export const allroutes = [
   { path: '/technology', element: <Technologies /> }, // Fixed path to match navbar
   { path: '/contact', element: <ContactUs /> },
   { path: '/about', element: <Visitus /> }, // Fixed path to match navbar
-  // Other routes can remain for direct access
-  { path: '/choose', element: <Choose /> },
-  { path: '/companyorg', element: <CompanyOrg /> },
-  { path: '/faq', element: <FAQ /> },
-  { path: '/testimonals', element: <Testimonal /> },
-  { path: '/content', element: <Content/>},
-
 ];
