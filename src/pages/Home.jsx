218937a968a8aecf1e5f71341ccf15d1bb1750ab
rohail jan <rohailjan726@gmail.com>
2025-05-14 17:24:18 +0500

    route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 
 
 import React from 'react'
-import Hero from '../components/Hero'
 import Services from '../components/Services'
 import Choose from '../components/Choose'
 import FAQ from '../components/FAQ'
@@ -13,7 +12,6 @@ import Content from '../components/Content'
 const Home = () => {
   return (
     <div>
-        <Hero/>
         <Content/>
         <Services/>
         <Industries/>
